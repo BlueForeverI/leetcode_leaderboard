1e3e32cfc508161c6c1b0c148327b925f4c0775a
Georgi Yolovski <georgi.yolovski@gannett.com>
2022-03-06 23:21:20 +0200

    Added force_ssl for prod

--- config/prod.exs
+++ config/prod.exs
@@ -43,7 +43,30 @@ config :logger, level: :info
 # We also recommend setting `force_ssl` in your endpoint, ensuring
 # no data is ever sent via http, always redirecting to https:
 #
-#     config :leetcode_leaderboard, LeetcodeLeaderboardWeb.Endpoint,
-#       force_ssl: [hsts: true]
-#
+# config :leetcode_leaderboard, LeetcodeLeaderboardWeb.Endpoint,
+#   force_ssl: [hsts: true]
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+host = System.get_env("PHX_HOST") || "example.com"
+port = String.to_integer(System.get_env("PORT") || "4000")
+
+config :leetcode_leaderboard, LeetcodeLeaderboardWeb.Endpoint,
+  url: [host: host, port: 443, scheme: "https"],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  http: [
+    # Enable IPv6 and bind on all interfaces.
+    # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+    # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+    # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+    ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    port: port
+  ],
+  secret_key_base: secret_key_base
+
 # Check `Plug.SSL` for all available options in `force_ssl`.
