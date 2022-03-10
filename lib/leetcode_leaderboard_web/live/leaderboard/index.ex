364e2ce6ae058c4e4a0ae84d4b9be58594f0a58e
georgiyolovski <georgi.yolovski@gannett.com>
2022-03-10 13:24:29 +0200

    Added a loading indicator

--- lib/leetcode_leaderboard_web/live/leaderboard/index.ex
+++ lib/leetcode_leaderboard_web/live/leaderboard/index.ex
@@ -13,6 +13,10 @@ defmodule LeetcodeLeaderboardWeb.Live.Index do
         <%= select f, :id, Enum.map(@problems, &({&1.title, &1.id})) %>
       </.form>
 
+      <%= if @loading do %>
+        <div>Loading...</div>
+      <% end %>
+
       <!-- <%= form_for :filter, "#", [phx_change: "update"], fn f -> %>
         <%= radio_button f, :all_submissions, false %>
         <%= label f, :all_submissions_false, "Leaderboard" %>
@@ -20,48 +24,51 @@ defmodule LeetcodeLeaderboardWeb.Live.Index do
         <%= radio_button f, :all_submissions, true %>
         <%= label f, :all_submissions_true, "All Submissions" %>
       <% end %> -->
-      <table>
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Date</th>
-            <th>Language</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <%= for row <- @rows do %>
+      <%= unless @loading do %>
+        <table>
+          <thead>
             <tr>
-              <td><%= row[:user] %></td>
-              <td><%= Calendar.strftime(row[:date], "%y-%m-%d %H:%M:%S")%></td>
-              <td><a href={row[:url]}><%= row[:lang] %></a></td>
+              <th>User</th>
+              <th>Date</th>
+              <th>Language</th>
             </tr>
-          <% end %>
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            <%= for row <- @rows do %>
+              <tr>
+                <td><%= row[:user] %></td>
+                <td><%= Calendar.strftime(row[:date], "%y-%m-%d %H:%M:%S")%></td>
+                <td><a href={row[:url]}><%= row[:lang] %></a></td>
+              </tr>
+            <% end %>
+          </tbody>
+        </table>
+      <% end %>
     </div>
     """
   end
 
   @impl true
   def mount(_params, %{}, socket) do
-    rows = Service.leaderboard()
     problems = Service.problems()
+
+    send(self(), {:initial_load})
 
     {
       :ok,
       socket
-      |> assign(:rows, rows)
+      |> assign(:rows, [])
       |> assign(:problems, problems)
       |> assign(:problem, problems |> List.last() |> Problem.changeset(%{}))
+      |> assign(:loading, true)
     }
   end
 
   @impl true
   def handle_event("update", %{"problem" => %{"id" => problem_id}}, socket) do
-    rows = Service.leaderboard(problem_id)
-
-    {:noreply, assign(socket, :rows, rows)}
+    send(self(), {:change_problem, problem_id})
+    {:noreply, assign(socket, :loading, true)}
   end
 
   def handle_event("update", %{"filter" => %{"all_submissions" => "true"} = filter}, socket) do
@@ -76,5 +83,25 @@ defmodule LeetcodeLeaderboardWeb.Live.Index do
     rows = Service.leaderboard(problem_id)
 
     {:noreply, assign(socket, :rows, rows) |> assign(:filter, filter)}
+  end
+
+  @impl true
+  def handle_info({:initial_load}, socket) do
+    rows = Service.leaderboard()
+
+    {
+      :noreply,
+      socket
+      |> assign(:rows, rows)
+      |> assign(:loading, false)
+    }
+  end
+
+  def handle_info({:change_problem, problem_id}, socket) do
+    {:noreply,
+     socket
+     |> assign(:loading, true)
+     |> assign(:rows, Service.leaderboard(problem_id))
+     |> assign(:loading, false)}
   end
 end
